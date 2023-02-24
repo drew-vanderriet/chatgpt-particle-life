899fc20e1787a877fd16d0eed6c30ea2ef3c3879
Drew Vanderriet <dvdriet@gmail.com>
2023-02-24 12:22:54 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,22 @@
-let particle;
+let particles = [];
 
 function setup() {
   createCanvas(400, 400);
-  particle = new Particle(200, 200, color(255, 0, 0));
+  // Add 50 red particles at random locations
+  for (let i = 0; i < 50; i++) {
+    let x = random(width);
+    let y = random(height);
+    let particle = new Particle(x, y, color(255, 0, 0));
+    particles.push(particle);
+  }
 }
 
 function draw() {
   background(220);
-  particle.update();
-  particle.draw();
+  
+  // Update and draw all particles
+  for (let particle of particles) {
+    particle.update();
+    particle.draw();
+  }
 }
