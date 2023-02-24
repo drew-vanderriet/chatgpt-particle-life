b5f3da7db9669019fbc53b62c5954fe9e8a9f7e4
Drew Vanderriet <dvdriet@gmail.com>
2023-02-24 13:11:18 -0500

    Update particles.js

--- particles.js
+++ particles.js
@@ -1,10 +1,10 @@
 class Particle {
-  static Colors = {
-    RED: [255, 0, 0],
-    BLUE: [0, 0, 255],
-    DARK_GREEN: [0, 100, 0],
-    ORANGE: [255, 165, 0],
-    PURPLE: [128, 0, 128]
+  static COLORS = {
+    RED: "red",
+    BLUE: "blue",
+    DARK_GREEN: "dark green",
+    ORANGE: "orange",
+    PURPLE: "purple",
   };
   
   constructor(x, y, c, attraction, repulsion) {
@@ -28,28 +28,64 @@ class Particle {
     this.acceleration.add(force);
   }
 
-  attract(other) {
+  interact(other) {
+    const color1 = this.color;
+    const color2 = other.color;
     const distance = this.position.dist(other.position);
+
+    // Predefined weights for each color combination
+    const weights = {
+      [Particle.COLORS.RED + Particle.COLORS.BLUE]: -0.5,
+      [Particle.COLORS.RED + Particle.COLORS.DARK_GREEN]: -0.2,
+      [Particle.COLORS.RED + Particle.COLORS.ORANGE]: 0.7,
+      [Particle.COLORS.RED + Particle.COLORS.PURPLE]: 0.9,
+      [Particle.COLORS.BLUE + Particle.COLORS.DARK_GREEN]: 0.4,
+      [Particle.COLORS.BLUE + Particle.COLORS.ORANGE]: -0.1,
+      [Particle.COLORS.BLUE + Particle.COLORS.PURPLE]: -0.8,
+      [Particle.COLORS.DARK_GREEN + Particle.COLORS.ORANGE]: 0.3,
+      [Particle.COLORS.DARK_GREEN + Particle.COLORS.PURPLE]: -0.6,
+      [Particle.COLORS.ORANGE + Particle.COLORS.PURPLE]: 0.1,
+    };
+
+    const key = [color1, color2].sort().join(" + ");
+    const weight = weights[key] || 0; // Default weight is 0 if color combination not defined
+
     if (distance > 0 && distance < 100) {
-      const strength = (this.attraction * other.attraction) / (distance * distance);
+      const strength = weight / (distance * distance);
       const force = p5.Vector.sub(other.position, this.position);
       force.setMag(strength);
       this.applyForce(force);
+      force.mult(-1);
+      other.applyForce(force);
     }
   }
-
-  repel(other) {
-    const distance = this.position.dist(other.position);
-    if (distance > 0 && distance < 100) {
-      const strength = (this.repulsion * other.repulsion) / (distance * distance);
-      const force = p5.Vector.sub(this.position, other.position);
-      force.setMag(strength);
-      this.applyForce(force);
+  
+  paintColor() {
+    let fillColor = "";
+    switch (this.color) {
+      case Particle.COLORS.RED:
+        fillColor = color(255, 0, 0);
+        break;
+      case Particle.COLORS.BLUE:
+        fillColor = color(0, 0, 255);
+        break;
+      case Particle.COLORS.DARK_GREEN:
+        fillColor = color(0, 100, 0);
+        break;
+      case Particle.COLORS.ORANGE:
+        fillColor = color(255, 165, 0);
+        break;
+      case Particle.COLORS.PURPLE:
+        fillColor = color(128, 0, 128);
+        break;
     }
+
+    return fillColor; 
   }
 
   draw() {
-    fill(this.color);
+    fill(paintColor());
+    noStroke(); // Remove the outline
     ellipse(this.position.x, this.position.y, this.radius, this.radius);
   }
 }
