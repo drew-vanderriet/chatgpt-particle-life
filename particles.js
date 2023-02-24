8081e9270fe0768599cad866412c310e3e8a6b07
Drew Vanderriet <dvdriet@gmail.com>
2023-02-24 14:01:12 -0500

    Update particles.js

--- particles.js
+++ particles.js
@@ -98,7 +98,7 @@ class Particle {
     noStroke(); // Remove the outline
     ellipse(this.position.x, this.position.y, this.radius, this.radius);
     stroke(255, 0, 0)
-    const vel = p5.Vector.mult(particle.velocity, 10);
+    const vel = p5.Vector.mult(this.velocity, 10);
     line(this.position.x, this.position.y, this.position.x + vel.x, this.position.y + vel.y)
   }
 }
