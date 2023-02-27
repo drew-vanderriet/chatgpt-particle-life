5f86fea74ae2a901ff34772358f3bb4ea0a80282
Drew Vanderriet <dvdriet@gmail.com>
2023-02-27 12:37:29 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,44 @@
 let particles = [];
+let weights;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  
+  // Predefined weights for each color combination
+  weights = {
+    [Particle.COLORS.RED + Particle.COLORS.RED]: random(-1, 1),
+    [Particle.COLORS.RED + Particle.COLORS.BLUE]: random(-1, 1),
+    [Particle.COLORS.RED + Particle.COLORS.DARK_GREEN]: random(-1, 1),
+    [Particle.COLORS.RED + Particle.COLORS.ORANGE]: random(-1, 1),
+    [Particle.COLORS.RED + Particle.COLORS.PURPLE]: random(-1, 1),
+    [Particle.COLORS.BLUE + Particle.COLORS.BLUE]: random(-1, 1),
+    [Particle.COLORS.BLUE + Particle.COLORS.RED]: random(-1, 1),
+    [Particle.COLORS.BLUE + Particle.COLORS.DARK_GREEN]: random(-1, 1),
+    [Particle.COLORS.BLUE + Particle.COLORS.ORANGE]: random(-1, 1),
+    [Particle.COLORS.BLUE + Particle.COLORS.PURPLE]: random(-1, 1),
+    [Particle.COLORS.DARK_GREEN + Particle.COLORS.DARK_GREEN]: random(-1, 1),
+    [Particle.COLORS.DARK_GREEN + Particle.COLORS.RED]: random(-1, 1),
+    [Particle.COLORS.DARK_GREEN + Particle.COLORS.BLUE]: random(-1, 1),
+    [Particle.COLORS.DARK_GREEN + Particle.COLORS.ORANGE]: random(-1, 1),
+    [Particle.COLORS.DARK_GREEN + Particle.COLORS.PURPLE]: random(-1, 1),
+    [Particle.COLORS.ORANGE + Particle.COLORS.ORANGE]: random(-1, 1),
+    [Particle.COLORS.ORANGE + Particle.COLORS.RED]: random(-1, 1),
+    [Particle.COLORS.ORANGE + Particle.COLORS.BLUE]: random(-1, 1),
+    [Particle.COLORS.ORANGE + Particle.COLORS.DARK_GREEN]: random(-1, 1),
+    [Particle.COLORS.ORANGE + Particle.COLORS.PURPLE]: random(-1, 1),
+    [Particle.COLORS.PURPLE + Particle.COLORS.PURPLE]: random(-1, 1),
+    [Particle.COLORS.PURPLE + Particle.COLORS.RED]: random(-1, 1),
+    [Particle.COLORS.PURPLE + Particle.COLORS.BLUE]: random(-1, 1),
+    [Particle.COLORS.PURPLE + Particle.COLORS.DARK_GREEN]: random(-1, 1),
+    [Particle.COLORS.PURPLE + Particle.COLORS.ORANGE]: random(-1, 1),
+  };
+  
   // Add 50 red particles at random locations
   for (let i = 0; i < 150; i++) {
     let x = random(width);
     let y = random(height);
     let randomColor = random(Object.values(Particle.COLORS));
-    let particle = new Particle(x, y, randomColor);
+    let particle = new Particle(x, y, randomColor, weights);
     particles.push(particle);
   }
 }
